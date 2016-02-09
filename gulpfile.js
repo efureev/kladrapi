86cc667d77abf976facfcc031408efba4da66801
efureev <efureev@yandex.ru>
2016-02-09 18:07:34 +0500

    fix core

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp'),
 	csso = require('gulp-csso'),
 	uglify = require('gulp-uglify'),
 	concat = require('gulp-concat'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	watch = require('gulp-watch');
 
 gulp.task('default', function () {
 
 	// Javascript
 	gulp.src([
-			'./kladr/js/core.js',
-			'./kladr/js/kladr.js',
-			'./kladr/js/kladr_zip.js'
-		])
+		'./kladr/js/core.js',
+		'./kladr/js/kladr.js',
+		'./kladr/js/kladr_zip.js'
+	])
 		.pipe(concat('jquery.kladr.js'))
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
@@ -22,4 +23,17 @@ gulp.task('default', function () {
 		.pipe(csso())
 		.pipe(rename('jquery.kladr.min.css'))
 		.pipe(gulp.dest('./dist/'));
+});
+
+
+
+
+gulp.task('watch', function () {
+
+	gulp.watch([
+		'./kladr/js/core.js',
+		'./kladr/js/kladr.js',
+		'./kladr/js/kladr_zip.js',
+		'./kladr/css/style.css'
+	], ['default']);
 });
--- kladr/js/core.js
+++ kladr/js/core.js
@@ -1,15 +1,46 @@
-(function ($, window) {
+(function ($) {
+
+	var
+		url = 'kladr-api.ru/api.php',
+		/**
+		 * Преобразует запрос из формата принятого в плагине в формат сервиса
+		 *
+		 * @param {{}} query Запрос в формате плагина
+		 * @returns {{}} Запрос в формате сервиса
+		 */
+		toApiFormat = function (query) {
+			var params = {},
+				fields = {
+					type: 'contentType',
+					name: 'query',
+					withParents: 'withParent'
+				};
+
+			if (query.parentType && query.parentId) {
+				params[query.parentType + 'Id'] = query.parentId;
+			}
+
+			for (var key in query) {
+				if (query.hasOwnProperty(key) && query[key]) {
+					params[fields.hasOwnProperty(key) ? fields[key] : key] = query[key];
+				}
+			}
+
+			return params;
+		};
+
+
 	$.kladr = {};
 
 	(function () {
-		var protocol = window.location.protocol == 'https:' ? 'https:' : 'http:';
+		var protocol = location.protocol == 'https:' ? 'https:' : 'http:';
 
 		/**
 		 * URL сервиса
 		 *
 		 * @type {string}
 		 */
-		$.kladr.url = protocol + '//kladr-api.ru/api.php';
+		$.kladr.url = protocol + '//' + url;
 	})();
 
 	/**
@@ -18,10 +49,10 @@
 	 * @type {{region: string, district: string, city: string, street: string, building: string}}
 	 */
 	$.kladr.type = {
-		region:   'region',   // Область
+		region: 'region',   // Область
 		district: 'district', // Район
-		city:     'city',     // Город
-		street:   'street',   // Улица
+		city: 'city',     // Город
+		street: 'street',   // Улица
 		building: 'building'  // Строение
 	};
 
@@ -31,9 +62,9 @@
 	 * @type {{city: number, settlement: number, village: number}}
 	 */
 	$.kladr.typeCode = {
-		city:       1, // Город
+		city: 1, // Город
 		settlement: 2, // Посёлок
-		village:    4  // Деревня
+		village: 4  // Деревня
 	};
 
 	/**
@@ -43,7 +74,7 @@
 	 * @returns {boolean}
 	 */
 	$.kladr.validate = function (query) {
-		var type = $.kladr.type;
+		var type = this.type;
 
 		switch (query.type) {
 			case type.region:
@@ -51,52 +82,43 @@
 			case type.city:
 				if (query.parentType && !query.parentId) {
 					error('parentId undefined');
-					return false;
 				}
 				break;
 			case type.street:
 				if (query.parentType != type.city) {
 					error('parentType must equal "city"');
-					return false;
 				}
 				if (!query.parentId) {
 					error('parentId undefined');
-					return false;
 				}
 				break;
 			case type.building:
 				if (!query.zip) {
 					if (!~$.inArray(query.parentType, [type.street, type.city])) {
 						error('parentType must equal "street" or "city"');
-						return false;
 					}
 					if (!query.parentId) {
 						error('parentId undefined');
-						return false;
 					}
 				}
 				break;
 			default:
 				if (!query.oneString) {
 					error('type incorrect');
-					return false;
 				}
 				break;
 		}
 
 		if (query.oneString && query.parentType && !query.parentId) {
 			error('parentId undefined');
-			return false;
 		}
 
 		if (query.typeCode && (query.type != type.city)) {
 			error('type must equal "city"');
-			return false;
 		}
 
 		if (query.limit < 1) {
 			error('limit must greater than 0');
-			return false;
 		}
 
 		return true;
@@ -111,12 +133,6 @@
 	$.kladr.api = function (query, callback) {
 		if (!callback) {
 			error('Callback undefined');
-			return;
-		}
-
-		if (!$.kladr.validate(query)) {
-			callback([]);
-			return;
 		}
 
 		var timeout = setTimeout(function () {
@@ -124,15 +140,14 @@
 			timeout = null;
 		}, 3000);
 
-		$.get($.kladr.url + "?callback=?",
+		$.get(this.url + "?callback=?",
 			toApiFormat(query),
 			function (data) {
 				if (timeout) {
 					callback(data.result || []);
 					clearTimeout(timeout);
 				}
-			},'jsonp'
-		);
+			}, 'jsonp');
 	};
 
 	/**
@@ -143,15 +158,13 @@
 	 * false если не существует.
 	 */
 	$.kladr.check = function (query, callback) {
-		if (!callback) {
+		if (!callback)
 			error('Callback undefined');
-			return;
-		}
 
 		query.withParents = false;
 		query.limit = 1;
 
-		$.kladr.api(query, function (objs) {
+		this.api(query, function (objs) {
 			objs && objs.length
 				? callback(objs[0])
 				: callback(false);
@@ -159,51 +172,11 @@
 	};
 
 	/**
-	 * Преобразует запрос из формата принятого в плагине в формат сервиса
-	 *
-	 * @param {{}} query Запрос в формате плагина
-	 * @returns {{}} Запрос в формате сервиса
-	 */
-	function toApiFormat(query) {
-		var params = {},
-			fields = {
-				type:        'contentType',
-				name:        'query',
-				withParents: 'withParent'
-			};
-
-		if (query.parentType && query.parentId) {
-			params[query.parentType + 'Id'] = query.parentId;
-		}
-
-		for (var key in query) {
-			if (hasOwn(query, key) && query[key]) {
-				params[hasOwn(fields, key) ? fields[key] : key] = query[key];
-			}
-		}
-
-		return params;
-	}
-
-	/**
-	 * Проверяет принадлежит ли объекту свойство
-	 *
-	 * @param {{}} obj Объект
-	 * @param {string} property Свойство
-	 * @returns {boolean}
-	 */
-	function hasOwn(obj, property) {
-		return obj.hasOwnProperty(property);
-	}
-
-	/**
 	 * Выводит ошибку на консоль
 	 *
 	 * @param {string} error Текст ошибки
 	 */
 	function error(error) {
-		var console = window.console;
-
-		console && console.error && console.error(error);
+		throw new Error(error);
 	}
-})(jQuery, window);
+})(jQuery);
